47fbf0bc715b52eb10a2e2324f0eb7ab38933ad7
AntonBrito <tonybrito85@gmail.com>
2022-11-09 11:24:38 -0500

    Create nav section for navigating between pages

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import { AiOutlineMenu } from "react-icons/ai"
+
+export default function Navbar() {
+  return (
+<div className="bg-slate-800 px-10 py-10">
+  <div>
+    <div>
+      <h1 className="text-5xl">
+        David P Hall
+      </h1>
+    </div>
+    <div>
+    <AiOutlineMenu />
+    </div>
+  </div>
+</div>
+  );
+}
